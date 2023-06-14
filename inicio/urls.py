a2c141680291ea57e27a959f981c6098c5f3db52
Nata360 <nataferre360@gmail.com>
2023-06-13 21:04:47 -0300

    nuevo intento

---
+++ inicio/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from inicio import views
+
+urlpatterns = [
+    path(''),
+    path('', views.inicio),
+    path('segunda-vista/', views.segunda_vista),
+    path('fecha-actual/', views.fecha_actual),
+    path('saludar/', views.saludar),
+    path('bienvenudo/<str:nombre>/<str:apellido>', views.bienvenudo),
+    path('crear-perro/<str:nombre>/<str:edad>', views.crear_perro),
+]
