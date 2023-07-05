81a512e361cdc56717e4c35aa440f408d6df2156
Nata360 <nataferre360@gmail.com>
2023-07-05 18:58:58 -0300

    Terminó la clase 22

--- inicio/form.py
+++ inicio/form.py
@@ -1,9 +1,14 @@
 from django import forms
 
-class CrearPerroFormulario(forms.Form):
+class PerroForularioBase(forms.Form):
     nombre = forms.CharField(max_length=20)
     edad = forms.IntegerField()
     
+class CrearPerroFormulario(PerroForularioBase):
+    ...
     
+class ModificarPerroFormulario(PerroForularioBase):
+    ...
+
 class BuscarPerroFormulario(forms.Form):
     nombre = forms.CharField(max_length=20, required=False)
--- inicio/urls.py
+++ inicio/urls.py
@@ -10,9 +10,20 @@ urlpatterns = [
     path('fecha-actual/', views.fecha_actual, name='fecha_actual'),
     path('saludar/', views.saludar, name='saludar'),
     path('bienvenudo/<str:nombre>/<str:apellido>', views.bienvenudo, name='bienvenudo'),
+    # Vistas comunes
+    
     # V1 Crear perro
     # path('crear-perro/<str:nombre>/<str:edad>', views.crear_perro, name='crear_perro'),
     # V2 Crear perro
-    path('perro/', views.listar_perros, name='listar_perros'),
-    path('perro/crear/', views.crear_perro, name='crear_perro'),
+    # path('perro/', views.listar_perros, name='listar_perros'),
+    # path('perro/crear/', views.crear_perro, name='crear_perro'),
+    # path('perro/eliminar/<int:perro_id>/', views.eliminar_perro, name='eliminar_perro'), #la info que se otorga en url, la vista lo recibe como parámetro
+    # path('perro/modificar/<int:perro_id>/', views.modificar_perro, name='modificar_perro'),
+     
+    # CBV
+    path('perro/', views.ListarPerros.as_view(), name='listar_perros'),
+    path('perro/crear/', views.CrearPerro.as_view(), name='crear_perro'),
+    path('perro/eliminar/<int:pk>/', views.EliminarPerro.as_view(), name='eliminar_perro'),
+    path('perro/modificar/<int:pk>/', views.ModificarPerro.as_view(), name='modificar_perro'),
+    path('perro/<int:pk>/', views.MostrarPerro.as_view(), name='mostrar_perro'),
 ]
